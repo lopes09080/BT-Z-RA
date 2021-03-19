e7d653b470534492d6ad46050f7b92bd8f447684
lopes09080 <80933335+lopes09080@users.noreply.github.com>
2021-03-19 00:31:55 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,177 @@
+const help = (prefix) => {
+	return `
+<══════════════════════>
+      *BØTZERA BOT*
+ <══════════════════════>
+
+__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█▄█████▄█   *DONO*  : LOPEES
+__█▼▼▼▼▼█ 
+_██ᏴϴͲ Ꮩ3.9██▌ 
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+
+┌─────────❶.❷────────
+║〘 INFORMAÇÕES 〙
+║
+╠🐉 * BØTZERA BOT* 
+╠🐉 𝐃𝐎𝐍𝐎:  ⃬⃗  Lopes
+╠🐉 *wa.me/+5511971351524*
+╠🐉 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+║
+╠══🐰〘 MENU 〙🐰══
+║
+║🐊 *${prefix}figu*
+║🐊 *${prefix}toimg*
+║🐊 *${prefix}darkjokes (memes aleatórios)*
+║🐊 *${prefix}tts*
+║🐊 *${prefix}lolih [on]*
+║🐊 *${prefix}url2img*
+║🐊 *${prefix}leens [na legenda]*
+║🐊 *${prefix}wait [na legenda]*
+║🐊 *${prefix}setprefix*
+║
+╠══🐰〘 OUTROS 〙🐰══
+║
+║🐊 *${prefix}linkgp*
+║🐊 *${prefix}simih [1/0]*
+║🐊 *${prefix}marcar*
+║🐊 *${prefix}banir [@]*
+║🐊 *${prefix}promover [@]*
+║🐊 *${prefix}rebaixar*
+║🐊 *${prefix}admins*
+║🐊 *${prefix}marcar2*
+║🐊 *${prefix}marcar3*
+║🐊 *${prefix}bloqueados*
+║🐊 *${prefix}limpar*
+║🐊 *${prefix}bc [ *texto* ]*
+║🐊 *${prefix}bemvindo [1/0]*
+║🐊 *${prefix}clonar [@]*
+║🐊 *${prefix}help1*
+║🐊 *${prefix}dono*
+║🐊 *${prefix}setfoto*
+║🐊 *${prefix}grupoinfo*
+║🐊 *${prefix}ytmp4*
+║🐊 *${prefix}bomdia*
+║🐊 *${prefix}boanoite*
+║🐊 *${prefix}marcar*
+║🐊 *${prefix}marcar2*
+║🐊 *${prefix}marcar3*
+║
+╠══🐰〘 IMAGENS 〙🐰══
+║
+║🐊 *${prefix}loli1*
+║🐊 *${prefix}hentai*
+║🐊 *${prefix}dono*
+║🐊 *${prefix}porno*
+║🐊 *${prefix}boanoite*
+║🐊 *${prefix}bomdia*
+║🐊 *${prefix}boatarde*
+║🐊 *${prefix}mia [aleatórias]*
+║🐊 *${prefix}rize [aleatórias]*
+║🐊 *${prefix}minato [aleatórias]*
+║🐊 *${prefix}boruto [aleatórias]*
+║🐊 *${prefix}hinata [aleatórias]*
+║🐊 *${prefix}sasuke [aleatórias]*
+║🐊 *${prefix}sakura [aleatórias]*
+║🐊 *${prefix}naruto [aleatórias]*
+║🐊 *${prefix}meme*   
+║🐊 *${prefix}lofi*
+║🐊 *${prefix}malkova*
+║🐊 *${prefix}canal*
+║🐊 *${prefix}nsfwloli1*
+║🐊 *${prefix}reislin*
+║
+╠══🐰〘 INTELIGÊNCIA 〙🐰══
+║
+║🐊 *${prefix}simih 1 (para ativar)*
+║🐊 *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+╠══🐰〘 GRUPO 〙🐰══
+║
+║🐊 *${prefix}banir*
+║🐊 *${prefix}leveling [on/off]*
+║🐊 *${prefix}level*
+║🐊 *${prefix}add*
+║🐊 *${prefix}promover*
+║🐊 *${prefix}setfoto [na legenda]*
+║🐊 *${prefix}setname [texto]*
+║🐊 *${prefix}rebaixar*
+║🐊 *${prefix}admins*
+║🐊 *${prefix}marcar*
+║🐊 *${prefix}marcar2*
+║🐊 *${prefix}marcar3*
+║🐊 *${prefix}bemvindo [1/0]*
+║🐊 *${prefix}grupoinfo*
+║🐊 *${prefix}bomdia*
+║🐊 *${prefix}boatarde*
+║🐊 *${prefix}boanoite*
+║
+╠══🐰〘 ESPECIFICO DO BOT 〙🐰══
+║
+║🐊 *${prefix}clonar [@]*
+║🐊 *${prefix}gay [@]*
+║🐊 *${prefix}map (nome)*
+║🐊 *${prefix}setppbot (marque uma img)*
+║🐊 *${prefix}pinterest (nome)*
+║🐊 *${prefix}desligar (so para o dono)*
+║🐊 *${prefix}timer*
+║
+╠══🐰〘 MAIS ALGUNS 〙🐰══
+║
+║🐊 *${prefix}neko*
+║🐊 *${prefix}testime*
+║🐊 *${prefix}tomp3*
+║🐊 *${prefix}modoanime [on/off]*
+║🐊 *${prefix}modonsfw [on/off]*
+║🐊 *${prefix}rize*
+║🐊 *${prefix}ytsearch*
+║🐊 *${prefix}moddroid [jogo/app]*
+║🐊 *${prefix}xvideos [titulo]**
+║🐊 *${prefix}nomegp*
+║🐊 *${prefix}darkjokes (memes aleatórios)*
+║🐊 *${prefix}gay1*
+║🐊 *${prefix}next*
+║🐊 *${prefix}alerta*
+║🐊 *${prefix}belle [img aleatórias]*
+║
+╠══🐰〘 COMANDOS DE VOZ 〙🐰══
+║
+║🐊 *${prefix}ola*
+║🐊 *${prefix}bv*
+║🐊 *${prefix}tchau*
+║🐊 *${prefix}bem*
+║🐊 *${prefix}a*
+║🐊 *${prefix}fdp*
+║🐊 *${prefix}onich*
+║🐊 *${prefix}beat1*
+║🐊 *${prefix}glub*
+║
+╠══🐰〘 OUTROS /2 〙🐰══
+║
+║🐊 *${prefix}antilink [1/0]*
+║🐊 *${prefix}brainly [pergunta]
+║🐊 *${prefix}setnomebot*
+║🐊 *${prefix}meme*
+║
+╠══🐰〘 INTERATIVOS 〙🐰══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║🐊 *bah*
+║🐊 *oii*
+║🐊 *bv*
+║🐊 *canta ai bot*
+║🐊 *grita*
+║
+║
+╚═〘 BØTZERA〙`
+}
+
+exports.help = help
+
